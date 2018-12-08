80ed196f3330ccd0caf97bded33405e65cf7b7f7
Stefan Fortuin <stefanfortuin02@gmail.com>
2018-12-08 10:51:17 +0100

    day 2_2 with some help from the internet :/

--- day2_2.py
+++ day2_2.py
@@ -2,43 +2,20 @@ lines = []
 with open("./Input/day2.txt") as f:
     for line in f:
         lines.append(line)
-           
-# it = 0
-# for line in lines:
-#     it += 1
-#     print(it)
 
-#     id = {}
-#     pos = 0
-#     for char in line:
-#         id[char] = pos
-#         pos += 1
-#         for lineToCheck in lines:
-#             idToCheck = {}
-#             posToCheck = 0
-#             for charToCheck in lineToCheck:
-#                 idToCheck[charToCheck] = posToCheck
-
-#                 # if id[char] == idToCheck[charToCheck] and char == charToCheck:
-#                 #     print
-
-#                 posToCheck += 1
-#                 print(idToCheck)
-positions = []
-
-for line in lines:
-    pos = 0
-    for char in line:
-        for lineToCheck in lines:
-            if len(lineToCheck) <= pos and lineToCheck[pos] != char:
-                print(line) 
-        
-        pos += 1
-
-for id in positions:
-    for char in id:
-        pos = id[char]
-
-
+for x in lines:
+	for y in lines:
+		diff = 0
+		for i in range(len(line)):
+			if x[i] != y[i]:
+				diff += 1
+		
+		if diff == 1:
+			ans = ''
+			for i in range(len(x)):
+				if x[i] == y[i]:
+					ans += str(x[i])
+			print(ans)
+			print(x,y)
 
 
